f0123e0b2e28ce009940d0cf28c5e1b5aad197b8
CHZelaya <chzelaya1201@gmail.com>
2025-02-13 22:16:36 -0700

    Finished initial design for dashboard menu, set up temporary logic to load drawers based on user credentials.

--- src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
+++ src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
@@ -11,10 +11,14 @@ import java.io.IOException;
 public class TEDesktopApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TEDesktopApp.class.getResource("/views/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(TEDesktopApp.class.getResource("/views/dashboard-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("Hello!");
         //stage.setMaximized(true);
+        //stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
