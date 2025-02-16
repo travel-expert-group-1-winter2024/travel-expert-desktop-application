0fc6a5035f34a5454bb1912535a26373446e4947
sorawat38 <st.sorawat38@gmail.com>
2025-02-16 12:42:13 -0700

    [feat] create simple login view

--- src/main/java/org/example/travelexpertdesktopapplication/controllers/LoginController.java
+++ src/main/java/org/example/travelexpertdesktopapplication/controllers/LoginController.java
@@ -1,4 +1,38 @@
+/**
+ * Sample Skeleton for 'logic-view.fxml' Controller Class
+ */
+
 package org.example.travelexpertdesktopapplication.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnLogin"
+    private Button btnLogin; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfPassword"
+    private TextField tfPassword; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tfUsername"
+    private TextField tfUsername; // Value injected by FXMLLoader
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnLogin != null : "fx:id=\"btnLogin\" was not injected: check your FXML file 'logic-view.fxml'.";
+        assert tfPassword != null : "fx:id=\"tfPassword\" was not injected: check your FXML file 'logic-view.fxml'.";
+        assert tfUsername != null : "fx:id=\"tfUsername\" was not injected: check your FXML file 'logic-view.fxml'.";
+
+    }
+
 }
