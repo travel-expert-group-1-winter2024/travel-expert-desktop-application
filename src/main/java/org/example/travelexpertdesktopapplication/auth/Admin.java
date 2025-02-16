4508c169b408cd2fade9c80257ddd6cc438796f5
sorawat38 <st.sorawat38@gmail.com>
2025-02-16 13:21:55 -0700

    [fix] remove role out in constructor

--- src/main/java/org/example/travelexpertdesktopapplication/auth/Admin.java
+++ src/main/java/org/example/travelexpertdesktopapplication/auth/Admin.java
@@ -2,7 +2,7 @@ package org.example.travelexpertdesktopapplication.auth;
 
 public class Admin extends User {
 
-    protected Admin(String firstName, String middleInitial, String lastName, String email, String phone, String username, String password, UserRole role) {
+    public Admin(String firstName, String middleInitial, String lastName, String email, String phone, String username, String password) {
         super(firstName, middleInitial, lastName, email, phone, username, password, UserRole.ADMIN);
     }
 }
