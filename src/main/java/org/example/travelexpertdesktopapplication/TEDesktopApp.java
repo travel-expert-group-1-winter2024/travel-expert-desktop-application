905478787ddd381ad6e7dc945d566a17b3f0cb43
sorawat38 <st.sorawat38@gmail.com>
2025-02-17 19:48:22 -0700

    [build] add testing connection when starting application

--- src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
+++ src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.example.travelexpertdesktopapplication.controllers.LoginController;
+import org.example.travelexpertdesktopapplication.dao.DatabaseManager;
 import org.example.travelexpertdesktopapplication.dao.UserDAO;
 import org.example.travelexpertdesktopapplication.services.AuthService;
 
@@ -14,6 +15,11 @@ import java.io.IOException;
 public class TEDesktopApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // test connection since the beginning of application
+        if (!DatabaseManager.testConnection()) {
+            System.exit(1);
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(TEDesktopApp.class.getResource("/views/logic-view.fxml"));
         fxmlLoader.setControllerFactory(param -> new LoginController(new AuthService(new UserDAO())));
         Scene scene = new Scene(fxmlLoader.load());
