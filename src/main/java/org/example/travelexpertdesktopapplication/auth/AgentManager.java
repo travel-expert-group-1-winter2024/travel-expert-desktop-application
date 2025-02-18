25bc9adce5502853d22f5b883b6d4cfe2b934fd2
sorawat38 <st.sorawat38@gmail.com>
2025-02-17 17:56:40 -0700

    [fix] change access modifier of Agent Manager

--- src/main/java/org/example/travelexpertdesktopapplication/auth/AgentManager.java
+++ src/main/java/org/example/travelexpertdesktopapplication/auth/AgentManager.java
@@ -2,7 +2,7 @@ package org.example.travelexpertdesktopapplication.auth;
 
 public class AgentManager extends User {
 
-    protected AgentManager(String firstName, String middleInitial, String lastName, String email, String phone, String username, String password) {
+    public AgentManager(String firstName, String middleInitial, String lastName, String email, String phone, String username, String password) {
         super(firstName, middleInitial, lastName, email, phone, username, password, UserRole.MANAGER);
     }
 }
