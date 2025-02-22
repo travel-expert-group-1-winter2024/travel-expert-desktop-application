41b7d71df33fd6164a5f7ceac32ce69e4aca8eb4
sorawat38 <st.sorawat38@gmail.com>
2025-02-22 15:02:26 -0700

    [feat] create session manager to keep tracking current user

---
+++ src/main/java/org/example/travelexpertdesktopapplication/auth/SessionManager.java
@@ -0,0 +1,23 @@
+package org.example.travelexpertdesktopapplication.auth;
+
+public class SessionManager {
+    private static SessionManager instance;
+    private User user;
+
+    private SessionManager() {}
+
+    public static SessionManager getInstance() {
+        if (instance == null) {
+            instance = new SessionManager();
+        }
+        return instance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
