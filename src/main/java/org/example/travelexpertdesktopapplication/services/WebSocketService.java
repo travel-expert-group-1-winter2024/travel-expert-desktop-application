618650d80ea3c852d40b1d998354bc9f74239d21
sorawat38 <st.sorawat38@gmail.com>
2025-02-21 22:11:43 -0700

    [feat] chat v2

--- src/main/java/org/example/travelexpertdesktopapplication/services/WebSocketService.java
+++ src/main/java/org/example/travelexpertdesktopapplication/services/WebSocketService.java
@@ -1,7 +1,6 @@
 package org.example.travelexpertdesktopapplication.services;
 
-import org.example.travelexpertdesktopapplication.models.ChatMessage;
-import org.example.travelexpertdesktopapplication.models.Greeting;
+import org.example.travelexpertdesktopapplication.models.Message;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.*;
 import org.springframework.web.socket.client.WebSocketClient;
@@ -10,6 +9,7 @@ import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.tinylog.Logger;
 
 import java.lang.reflect.Type;
+import java.time.LocalDateTime;
 import java.util.function.Consumer;
 
 public class WebSocketService {
@@ -30,39 +30,48 @@ public class WebSocketService {
         stompClient.connect(serverUri, new StompSessionHandlerAdapter() {
             @Override
             public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+                Logger.info("New session established : " + session.getSessionId());
                 stompSession = session;
-                Logger.info("Connected to WebSocket server");
-                stompSession.subscribe("/topic/greetings", new StompFrameHandler() {
-                    @Override
-                    public Type getPayloadType(StompHeaders headers) {
-                        return Greeting.class;  // Specify message type
-                    }
-
-                    @Override
-                    public void handleFrame(StompHeaders headers, Object payload) {
-                        if (payload instanceof Greeting) {
-                            Greeting greeting = (Greeting) payload;
-                            Logger.info("Received message from server: " + greeting.getContent());
-                            if (onMessageReceived != null) {
-                                onMessageReceived.accept(greeting.getContent());
-                            }
-                        } else {
-                            Logger.error("Unexpected payload type: " + payload.getClass());
-                        }
-                    }
-                });
+                stompSession.subscribe("/topic/messages", this);
             }
 
             @Override
-            public void handleTransportError(StompSession session, Throwable exception) {
+            public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
                 Logger.error(exception, "Error occurred while connecting to the server");
+            }
+
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return Message.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                if (payload instanceof Message) {
+                    Message msg = (Message) payload;
+                    Logger.info("Received : " + msg.getText() + " from : " + msg.getFrom());
+                    if (onMessageReceived != null) {
+                        onMessageReceived.accept(createDisplayMessage(msg));
+                    }
+                } else {
+                    Logger.error("Unexpected payload type: " + payload.getClass());
+                }
             }
         });
     }
 
     public void sendMessage(String message) {
         if (stompSession != null && stompSession.isConnected()) {
-            stompSession.send("/app/hello", new ChatMessage(message));
+            Message msg = new Message();
+            msg.setFrom("Nicky"); // TODO: Replace with user name
+            msg.setText(message);
+            stompSession.send("/app/chat", msg);
+            Logger.info("Message sent to websocket server");
         }
+    }
+
+    private String createDisplayMessage(Message message) {
+        LocalDateTime now = LocalDateTime.now();
+        return now.getHour() + ":" + now.getMinute() + " [" + message.getFrom() + "]: " + message.getText();
     }
 }
