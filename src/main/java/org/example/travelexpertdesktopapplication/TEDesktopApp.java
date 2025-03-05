a27714621a51c00b7040b3e47333c09ea1ae4d78
CHZelaya <chzelaya1201@gmail.com>
2025-03-04 21:25:41 -0700

    [fix] Fixed Agent/Agencies views, layers were placed in wrong order, putting the horizontal buttons behind other elements making them unclickable

--- src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
+++ src/main/java/org/example/travelexpertdesktopapplication/TEDesktopApp.java
@@ -20,40 +20,6 @@ public class TEDesktopApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        //! Havent removed this old code yet, haven't decided if I still need it or not.
-//        //Loading the dashboard view and controller
-//        FXMLLoader dashboardLoader = new FXMLLoader(getClass().getResource("/views/dashboard-view.fxml"));
-//        //Grabbing the root element in the view, such as a AnchorPane or Vbox.
-//        Parent dashboardRoot = dashboardLoader.load();
-//        DashboardController dashboardController = dashboardLoader.getController();
-//
-//        //Repeating the steps to grab the Agent view and drawer
-//        FXMLLoader agentDrawerLoader = new FXMLLoader(getClass().getResource("/views/agent-drawer-view.fxml"));
-//        Parent agentDrawerRoot = agentDrawerLoader.load();
-//        AgentDrawerController agentDrawerController = agentDrawerLoader.getController();
-//
-//
-//        //Repeating to grab the Manager view and drawer
-//        FXMLLoader managerDrawerLoader = new FXMLLoader(getClass().getResource("/views/manager-drawer-view.fxml"));
-//        Parent managerDrawerRoot = managerDrawerLoader.load();
-//        ManagerDrawerController managerDrawerController = managerDrawerLoader.getController();
-//
-//        FXMLLoader dashboardButtonLoader = new FXMLLoader(getClass().getResource("/views/dashboard-button-view.fxml"));
-//        Parent dashboardButtonRoot = dashboardButtonLoader.load();
-//        DashboardButtonController dashboardButtonController = dashboardButtonLoader.getController();
-//
-//        //Instantiating the CentralController and setting the references to the other controllers
-//        dashboardController.setAgentDrawerController(agentDrawerController);
-//        dashboardController.setManagerDrawerController(managerDrawerController);
-//        dashboardController.setDashboardButtonController(dashboardButtonController);
-//
-//        // Inject the DashboardController into the AgentDrawerController
-//        agentDrawerController.setDashboardController(dashboardController);
-//        managerDrawerController.setDashboardController(dashboardController);
-//        dashboardButtonController.setDashboardController(dashboardController);
-
-
-
         // test connection since the beginning of application
         if (!DatabaseManager.testConnection()) {
             System.exit(1);
